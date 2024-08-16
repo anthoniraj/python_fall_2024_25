11f63fa7f9f8e843ab5d652e84ef19a6325bae08
anthoniraj <anthoniraja@gmail.com>
2024-08-16 15:37:14 +0530

    added basics python scripts

---
+++ basics/add_using_input.py
@@ -0,0 +1,4 @@
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+sum = int(first_number) + int(second_number)
+print(f"The addition is {sum}")
---
+++ basics/first_demo.py
@@ -0,0 +1,6 @@
+first_number = 10
+second_number = 20
+sum = first_number + second_number
+#print(first_number + second_number)
+print(f"The addition of {first_number} 
+      and {second_number} is {sum}")
